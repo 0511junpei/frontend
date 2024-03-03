17cdf8810f11d03a4e940538192aecac421c3bb9
0511junpei <shok2gok.tdos3ky@icloud.com>
2024-03-03 22:38:11 +0900

    フロント開発中（Layout作成）

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import { Alert, Button } from "reactstrap";
+
 const index = () => {
-    return (
-        <div>
-            Hello World
-        </div>
-    );
-}
+  return (
+    <div>
+      <div>
+        <Alert color="primary">Hello Project</Alert>
+        <Button color="primary">Hello from nextjs</Button>
+      </div>
+    </div>
+  );
+};
 
 export default index;
